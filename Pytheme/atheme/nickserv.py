a689be4244c99bd1a494844708f7ad2c16988062
Sam Hoffman <samuelhoffman2@gmail.com>
2018-05-24 01:50:22 -0500

    xmlrpc: basic nickserv set commands for account management

--- Pytheme/atheme/nickserv.py
+++ Pytheme/atheme/nickserv.py
@@ -3,3 +3,15 @@ from .service import nickserv_command
 
 def info(account):
     return nickserv_command("info", account)
+
+
+def set_password(new_password):
+    return nickserv_command("set", f"password {new_password}")
+
+
+def set_email(new_email):
+    return nickserv_command("set", f"email {new_email}")
+
+
+def set_accountname(new_acct_name):
+    return nickserv_command("set", f"accountname {new_acct_name}")
