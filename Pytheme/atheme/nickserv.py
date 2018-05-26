e882a05ac85ea8867160b22801345d077acc24fa
dialupmodem <c66029@gmail.com>
2018-05-26 06:02:25 -0400

    added quick and dirty nickserv parsing, styled nickserv template, display basic account info, added footer

--- Pytheme/atheme/nickserv.py
+++ Pytheme/atheme/nickserv.py
@@ -1,8 +1,11 @@
 from .service import nickserv_command
+from re import search
 
 
 def info(account):
-    return nickserv_command("info", account)
+    data = nickserv_command("info", account)
+    return NickservInfo(data=data)
+
 
 
 def set_password(new_password):
@@ -15,3 +18,69 @@ def set_email(new_email):
 
 def set_accountname(new_acct_name):
     return nickserv_command("set", f"accountname {new_acct_name}")
+
+class NickservInfo:
+  def __init__(self, data = None):
+    if data is None:
+      return
+    self.parse_info(data)
+    
+  def parse_info(self, data):
+    lines = data.split("\n")
+
+    for line in lines:
+
+      match = search("Registered\s+: (\w+ \d\d (?:\d\d|:)+ \d{4}) \+\d{4} (\((?:(?:\d+|\w)+\s)+ago\))", line)
+      if match is not None:
+        self.registered_on = match.group(1)
+        self.registered_since = match.group(2)
+        continue
+      
+      match = search("User reg\.\s+: (\w+ \d\d (?:\d\d|:)+ \d{4}) \+\d{4} (\((?:(?:\d+|\w)+\s)+ago\))", line)
+      if match is not None:
+       self.user_registered_on = match.group(1)
+       self.user_registered_since = match.group(2)
+       continue
+      
+      match = search("Entity ID\s+:\s((?:\w|\d)+)", line)
+      if match is not None:
+       self.entity_id = match.group(1)
+       continue
+
+      match = search("Last seen\s+: (\w+ \d\d (?:\d\d|:)+ \d{4}) \+\d{4} (\((?:(?:\d+|\w)+\s)+ago\))", line)
+      if match is not None:
+       self.last_seen = match.group(1)
+       self.last_seen_since = match.group(2)
+       continue
+
+      match = search("Nicks\s+: (.*)", line)
+      if match is not None:
+       self.nicks = match.group(1)
+       continue
+
+      match = search("Email\s+: (.*@.*\.\w+)", line)
+      if match is not None:
+       self.email = match.group(1)
+       continue
+      
+      match = search("Flags\s+: ((?:\w+|\,\s))+", line)
+      if match is not None:
+       self.flags = match.group(1)
+       continue
+
+      match = search("Channels\s+: ((?:\d \w+|\,\s))+", line)
+      if match is not None:
+       self.channels = match.group(1)
+       continue
+
+      match = search("Groups\s+: (\!(?:\w+|-|_)+|,\s)+", line)
+      if match is not None:
+       self.groups = match.groups(1)      
+       continue
+
+      
+
+      
+
+
+  
